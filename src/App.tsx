3d61ce4150c69393f0b3bc1144f567aba6773e4d
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-05-30 19:14:54 +0600

    Making Navbar Style changes.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
     <>
       <nav className="flex items-center justify-around m-5">
         <div className="text-6xl font-bold font-serif text-green-800">2048</div>
-        <div className="flex text-2xl text-green-500 font-mono font-bold">
-          <div>Score</div>
-          <div>Best Score</div>
+        <div className="flex text-2xl text-green-500 font-mono font-bold w-xs align-middle justify-center">
+          <div className="w-1/2 text-center">Score</div>
+          <div className="w-1/2 text-center">Best Score</div>
         </div>
         <div className="">New Game</div>
       </nav>
