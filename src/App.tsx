3026386360a5cdcf3e5c886f6d87f1cd45e2a506
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-05-30 18:55:12 +0600

    Working on Navbar style.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ function App() {
     <>
       <nav className="flex items-center justify-around m-5">
         <div className="text-6xl font-bold font-serif text-green-800">2048</div>
-        <div>score bestscore</div>
+        <div className="flex justify-between">
+          <div>Score</div>
+          <div>Best Score</div>
+        </div>
         <div>New Game</div>
       </nav>
     </>
