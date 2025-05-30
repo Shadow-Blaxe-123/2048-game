9840fe8077923ba2dd9d91ed8667caeff04d1940
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-05-30 19:01:12 +0600

    Navbar Styles.

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ function App() {
   return (
     <>
       <nav className="flex items-center justify-around m-5">
-        <div className="text-6xl font-bold font-serif text-green-800">2048</div>
-        <div className="flex text-2xl">
+        <div className="text-6xl font-bold font-serif text-green-800 w-1/3">
+          2048
+        </div>
+        <div className="flex text-2xl text-green-500 font-mono font-bold w-1/3">
           <div>Score</div>
           <div>Best Score</div>
         </div>
-        <div>New Game</div>
+        <div className="w-1/3">New Game</div>
       </nav>
     </>
   );
