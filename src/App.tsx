cc409f15c2ab2ae906b45d73205da9351a95a9db
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-01 14:29:17 +0600

    Patched Score and Best Score.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,14 @@ function App() {
       <nav className="flex items-center justify-around m-5">
         <div className="font-serif text-6xl font-bold text-green-800">2048</div>
         <div className="flex justify-center font-mono text-2xl font-bold text-green-500 align-middle w-xs">
-          <div className="w-1/2 text-center">Score</div>
-          <div className="w-1/2 text-center">Best Score</div>
+          <div className="w-1/2 text-center">
+            <div>Score</div>
+            <div>0</div>
+          </div>
+          <div className="w-1/2 text-center">
+            <div>Best Score</div>
+            <div>0</div>
+          </div>
         </div>
         <div className="p-2 text-3xl font-black text-blue-500 border-2 hover:cursor-pointer rounded-2xl hover:bg-blue-200">
           New Game
