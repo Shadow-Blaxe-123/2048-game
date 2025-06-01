4830dd2bbde417ad9516b7d2b7576eca93c76a6e
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-01 15:49:46 +0600

    Added a TODO.

--- src/Board.tsx
+++ src/Board.tsx
@@ -4,6 +4,8 @@ import Tile from "./Tile";
 export default function Board() {
   const board = useBoardStore((state) => state.board);
 
+  // TODO: Fix the styles so that the page will stop scrolling when arrowUP and arrowDown are pressed.
+
   return (
     <div className="bg-[#bbada0] p-4 rounded-lg inline-grid grid-cols-4 gap-2">
       {board.map((row, rowIndex) =>
