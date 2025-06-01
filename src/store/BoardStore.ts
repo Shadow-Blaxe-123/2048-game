d894efa54066a7ff96b445af1726ea5dfb508d22
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-01 15:29:30 +0600

    Made the board state as a Zustand state.

---
+++ src/store/BoardStore.ts
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+export const useBoardStore = create(() => ({
+  board: [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+}));
