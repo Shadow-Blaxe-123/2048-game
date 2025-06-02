a7062a0f19de32d468f65e71a2cf580b1b71cfa8
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-02 21:01:23 +0600

    Added a basic resetBoard function to Global State with Zustand.

--- src/store/BoardStore.ts
+++ src/store/BoardStore.ts
@@ -1,11 +1,28 @@
 import { create } from "zustand";
 
+type State = {
+  board: number[][];
+};
+
+type SetState = {
+  resetBoard: () => void;
+};
+
 // Making the state.
-export const useBoardStore = create(() => ({
+export const useBoardStore = create<State & SetState>((set) => ({
   board: [
     [0, 0, 2, 0],
     [0, 0, 0, 0],
     [0, 0, 32, 0],
     [0, 0, 0, 0],
   ],
+  resetBoard: () =>
+    set({
+      board: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+    }),
 }));
