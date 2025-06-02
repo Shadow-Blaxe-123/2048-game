fb38dc0bca6dea2b62fe6112941093ad502bcf13
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-02 19:55:53 +0600

    Checking how the tiles renders when Board has values that are not zero.

--- src/store/BoardStore.ts
+++ src/store/BoardStore.ts
@@ -3,9 +3,9 @@ import { create } from "zustand";
 // Making the state.
 export const useBoardStore = create(() => ({
   board: [
+    [0, 0, 2, 0],
     [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [0, 0, 32, 0],
     [0, 0, 0, 0],
   ],
 }));
