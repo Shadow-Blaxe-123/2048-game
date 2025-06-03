dfc1355899cf1825b3d6ee3f06a803739533b595
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-06-03 16:00:18 +0600

    Made the New Game btn make the score reset to Zero.

--- src/store/BoardStore.ts
+++ src/store/BoardStore.ts
@@ -12,12 +12,16 @@ type SetState = {
 
 // Making the state.
 export const useBoardStore = create<State & SetState>((set) => ({
+  // State Values:
   board: [
     [0, 0, 2, 0],
     [0, 0, 0, 0],
     [0, 0, 32, 0],
     [0, 0, 0, 0],
   ],
+  bestScore: 0,
+  score: 0,
+  // SetState Functions:
   resetBoard: () =>
     set({
       board: [
@@ -26,7 +30,6 @@ export const useBoardStore = create<State & SetState>((set) => ({
         [0, 0, 0, 0],
         [0, 0, 0, 0],
       ],
+      score: 0,
     }),
-  bestScore: 0,
-  score: 0,
 }));
